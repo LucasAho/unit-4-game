4e2ad241b2f31c4c35bf334bccb151057609f0d1
Lucas Aho <aholucas11@gmail.com>
2019-10-23 17:15:37 -0600

    index file made and js psuedocode written

---
+++ Assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Four crystals display as buttons
+//Player shown random number
+//Assign values to crystals randomly
+//update score counter
+//player win
+//player lose
+//restart
+    //not with refresh
