28a54dd3080f068aa32bf2e4e249ee2b25383923
Lucas Aho <aholucas11@gmail.com>
2019-10-23 19:40:42 -0600

    Set 4 random numbers and asigned them to 4 gems

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -6,3 +6,36 @@
 //player lose
 //restart
     //not with refresh
+
+
+//Lets begin by console logging five random variables
+var gemValues = {
+    gemOne: 0,
+    gemTwo: 0,
+    gemThree: 0,
+    gemFour: 0,
+    gemSet: function() {
+        var numHold = [0,0,0,0];
+        for (var i = 0; i <numHold.length; i++) {
+            numHold[i] = Math.floor(Math.random()*12)+1;
+        }
+        this.gemOne = numHold[0];
+        this.gemTwo = numHold[1];
+        this.gemThree = numHold[2];
+        this.gemFour = numHold[3];
+        console.log(numHold);
+    },
+    
+    
+}
+gemValues.gemSet();
+console.log(gemValues.gemOne);
+
+
+
+
+    
+
+
+//gems:1-12
+//guess:18-120
