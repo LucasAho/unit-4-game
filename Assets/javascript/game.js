229b0196e9564fb1b757554b9337d0813d0ea666
Lucas Aho <aholucas11@gmail.com>
2019-10-25 12:18:53 -0600

    Added pseudocode to all files for readability, removed reset css, and wrote README

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,12 +1,3 @@
-//Four crystals display as buttons
-//Player shown random number
-//Assign values to crystals randomly
-//update score counter
-//player win
-//player lose
-//restart
-    //not with refresh
-
 //Game values
 var numGoal = 0;
 var wins = 0;
@@ -14,7 +5,6 @@ var losses = 0;
 var playerScore = 0;
 
 //Writes player scores to page
-
 $("#playerScore").html(playerScore);
 $("#scoreWin-text").html(" " + wins);
 $("#scoreLoss-text").html(" " + losses);
@@ -25,6 +15,7 @@ var gemValues = {
     gemTwo: 0,
     gemThree: 0,
     gemFour: 0,
+    //gemSet assigns random values to each gem
     gemSet: function() {
         var numHold = [0,0,0,0];
         for (var i = 0; i <numHold.length; i++) {
@@ -36,77 +27,70 @@ var gemValues = {
         this.gemFour = numHold[3];
         
     },  
+    //startGoal assigns an initial value to the player's goal score
     startGoal: function() {
         if (numGoal === 0) {
         numGoal = Math.floor(Math.random()*121)+18;
         $("#scoreSet-text").html(numGoal);
         return numGoal;
         }
-    },   
+    }   
 }
+//Reset function gives new values to gems and goal to beat
 function reset() {
     gemValues.gemSet();
     gemValues.startGoal();
     playerScore = 0;
-    numGoal = Math.floor(Math.random()*121)+18;
+    numGoal = Math.floor(Math.random()*121)+19;
     $("#scoreSet-text").html(numGoal);
     $("#playerScore").html(playerScore);
 }
+//Start function runs at the very beginning of the page
 function start() {
     gemValues.startGoal();
     gemValues.gemSet();
 }
-if (wins === 0) {
+//Runs start function if player has not finished a round
+if (wins === 0 && losses === 0) {
     start();
 }
 
-function scoreCheck() {
-    
+//Score check looks if players have added enough points to win or lose the game
+function scoreCheck() {    \
+    //If player has won the game, this runs
     if (playerScore === numGoal){        
         wins++;
         reset();
         $("#winlossNote").html("You won!!!<br>");
+    //If player has reached lose condition, this runs
     } else if (playerScore > numGoal){        
         losses++;
         reset();
         $("#winlossNote").html("You lost!!!<br>");
     }
+    //This updates html text4
     $("#scoreWin-text").html(" " + wins);
     $("#scoreLoss-text").html(" " + losses);
 }
 
-//Adds to player score based on button clicked
+//These assign value to each gem button and update player's score
 $("#gemSlot1").click(function(){
     playerScore= playerScore + gemValues.gemOne;
     $("#playerScore").html(playerScore);
     scoreCheck();    
-})
+});
 $("#gemSlot2").click(function(){
     playerScore= playerScore + gemValues.gemTwo;
     $("#playerScore").html(playerScore);
     scoreCheck(); 
-})
+});
 $("#gemSlot3").click(function(){
     playerScore= playerScore + gemValues.gemThree;
     $("#playerScore").html(playerScore);
     scoreCheck(); 
-})
+});
 $("#gemSlot4").click(function(){
     playerScore= playerScore + gemValues.gemFour;
     $("#playerScore").html(playerScore);
     scoreCheck();     
-})
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+});
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# unit-4-game
-This is the repo for the fourth homework assignment in bootcamp
+#Crystal Collector
+A game where players must blindly add up their number by clicking different crystals in efforts to get it equal to a goal number which is randomly generated.
+
+##Game Description
+When players reach this score, they are either given a point towards their wins and a message displaying that they won, or if they go over the goal score, they are given a message showing that they lost, and their number of losses increases. At this point, the game will assign a new number to reach and new values to the crystals. It self resets all scores except the win and loss count, but these will be lost if the page is refreshed. 
+
+##File Infrastructure
+(index.html) contains the instructions and html framework of the game, as well as links to Bootstrap, Jquery, the javascript external file, and the css external file
+
+(Assets\javascript\game.js) contains the actual logic involved in the game and controls assigning gems values, counting up player scores, and resetting the game upon win or loss conditions
+
+(Assets\images) contains 4 images used as buttons when adding up a score
+
+(Assets\css\style.css) contains the typography and visual settings of any HTML sections not set with bootstrap
+
